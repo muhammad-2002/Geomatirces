b925dce68e5946a6bea636d89bb9b19e074d887a
muhammad-2002 <masumbilla10104@gmail.com>
2024-02-16 17:41:29 +0600

    Committed

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 
 
-//input function
-function getInput (UserInput){
-    const getID =document.getElementById(UserInput)
-    const getValue = getID.value
-    const parseflot = parseFloat(getValue)
-    return parseflot
-
-}
 
 //calculate Triangle
 
@@ -38,6 +30,50 @@ function calculateParallelogram(){
     
     return setId
     
+}
+//calculate Rhombus
+function calculateRhombus(){
+    const d1 = getInput('d1')
+    const d2= getInput('d2')
+    const result = 0.5*d1*d2
+    const setId = setValue('Rhombus', result,'setAttribute')
+    
+    return setId
+    
+}
+//calculate pentagon
+function CalculatePentagon(){
+    const p = getInput('p')
+    const b = getInput('b')
+
+   
+    const result = 0.5*p*b
+    
+    const setId = setValue('Pentagon', result,'setAttribute')
+    
+    return setId
+    
+}
+
+//calculate Ellipis
+function calculateEllips(){
+    const a = getInput('a')
+    const b= getInput('b')
+    const result = 1.41*a*b
+    const setId = setValue('Ellipse', result,'setAttribute')
+    
+    return setId
+    
+}
+
+
+//input function
+function getInput (UserInput){
+    const getID =document.getElementById(UserInput)
+    const getValue = getID.value
+    const parseflot = parseFloat(getValue)
+    return parseflot
+
 }
 
 //set value
